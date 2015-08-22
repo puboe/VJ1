ef3584be99dde15edac885732ed973f7fd6a50f9
unknown <riveign@gmail.com>
2015-08-22 19:19:13 -0300

    initial commit

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+	
+	private Animator animator;
+	bool ladder;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+		ladder = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ladder)
+			transform.Translate (Vector2.down * 0.5f * Time.smoothDeltaTime);
+		else
+			transform.Translate (Vector2.right * 0.5f * Time.smoothDeltaTime);
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		BoxCollider2D collider = other.gameObject.GetComponent<BoxCollider2D> ();
+		BoxCollider2D tCollider = this.gameObject.GetComponent<BoxCollider2D> ();
+		if(other.gameObject.name == "TopLadder"){
+			ladder = collider.bounds.min.x < tCollider.bounds.min.x && tCollider.bounds.max.x < collider.bounds.max.x;
+			animator.SetBool ("Ladder", ladder);
+		}
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D coll){
+		ladder = false;
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if(other.gameObject.name == "BottomLadder"){
+			this.GetComponent<Rigidbody2D>().isKinematic = false;
+		}
+	}
+	
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Animator animator;
+	bool climbing;
+	bool ground;
+	bool topLadder;
+	bool jumping;
+	float maxJumpHeight = 0.5f;
+	float jumpSpeed = 7.0f;
+	float fallSpeed = 12.0f;
+	public Vector2 groundPos;
+	
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+		climbing = false;
+		ground = true;
+		topLadder = false;
+		jumping = false;
+		groundPos = transform.position;
+		maxJumpHeight = transform.position.y + maxJumpHeight;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.D) && ground) {
+			transform.Translate (Vector2.right * 0.5f * Time.smoothDeltaTime);
+			animator.SetInteger ("Move", 1);
+		} else if (Input.GetKey (KeyCode.A) && ground) {
+			animator.SetInteger ("Move", 2);
+			transform.Translate (Vector2.left * 0.5f * Time.smoothDeltaTime);
+		} else if (Input.GetKey (KeyCode.W) && climbing) {
+			animator.SetInteger ("Move", 0);
+			ground = false;
+			transform.Translate (Vector2.up * 0.5f * Time.smoothDeltaTime);
+		} else if (Input.GetKey (KeyCode.S) && (!ground || topLadder)) {
+			animator.SetInteger ("Move", 0);
+			transform.Translate (Vector2.down * 0.5f * Time.smoothDeltaTime);
+		} else if (Input.GetKey (KeyCode.X)&& ground  && !jumping) {
+			jumping = true;
+			StartCoroutine("Jump");
+		} 
+
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		BoxCollider2D collider = other.gameObject.GetComponent<BoxCollider2D> ();
+		BoxCollider2D tCollider = this.gameObject.GetComponent<BoxCollider2D> ();
+		if(other.gameObject.name == "BottomLadder")
+			climbing = collider.bounds.min.x < tCollider.bounds.min.x && tCollider.bounds.max.x < collider.bounds.max.x;
+		if(other.gameObject.name == "TopLadder"){
+			ground = collider.bounds.min.y < tCollider.bounds.min.y && tCollider.bounds.max.y < collider.bounds.max.y;
+			climbing = collider.bounds.min.x < tCollider.bounds.min.x && tCollider.bounds.max.x < collider.bounds.max.x;
+			topLadder = true;
+		}
+
+	}
+
+	void OnTriggerEnter2D (Collider2D coll){
+		if(coll.gameObject.name == "Ground"){
+			ground = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D coll){
+		if(coll.gameObject.name == "TopLadder"){
+			topLadder = false;
+		}
+	}
+
+	IEnumerator Jump()
+	{
+		while(true)
+		{
+			if(transform.position.y >= maxJumpHeight)
+				jumping = false;
+			if(jumping){
+				transform.Translate(Vector2.up * jumpSpeed * Time.smoothDeltaTime);
+			}
+			else 
+			{
+				transform.position = Vector2.Lerp(transform.position, groundPos, fallSpeed * Time.smoothDeltaTime);
+				if((Vector2)transform.position == (Vector2)groundPos)
+					StopAllCoroutines();
+			}
+			
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+}
