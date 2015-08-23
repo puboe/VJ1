c42a353b1b3646553a25d442c64cb58eaa642f1d
Pablo Diaz <pablo.diazuboe@mercadolibre.com>
2015-08-23 00:31:56 -0300

    Barrel animations improved

---
+++ DonkeyKong/Assets/Scripts/Barrel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+
+	private Animator animator;
+	private Vector2 movement;
+	bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.G)) {
+			animator.SetInteger ("Roll", 1);
+			//this.transform.Translate(Vector2.down);
+		} 
+		if (Input.GetKey(KeyCode.H)) {
+			animator.SetInteger("Roll", 0);
+			//this.transform.Translate(Vector2.right);
+		}
+	}
+}
