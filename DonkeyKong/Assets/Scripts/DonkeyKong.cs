4fb2ba8883f0a223a75ee6e82cd59f7dbd481e79
Pablo Diaz <pablo.diazuboe@mercadolibre.com>
2015-08-23 04:26:05 -0300

    Donkey Kong basic animations

---
+++ DonkeyKong/Assets/Scripts/DonkeyKong.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DonkeyKong : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Ejecutar cada un intervalo de tiempo (depende de cuando se tiren los barriles).
+		if (Input.GetKey (KeyCode.X)) {
+			animator.SetInteger ("Throw", 1);
+		} else {
+			animator.SetInteger ("Throw", 0);
+		} 
+	}
+}
