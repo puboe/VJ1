5a6d228a755daeb5750752dc1811258cefb93a9b
Pablo Diaz <pablo.diazuboe@mercadolibre.com>
2015-08-25 16:26:41 -0300

    Menu button in main scene

--- DonkeyKong/Assets/Scripts/GameManager.cs
+++ DonkeyKong/Assets/Scripts/GameManager.cs
@@ -7,10 +7,11 @@ public class GameManager : MonoBehaviour {
 	public static GameManager instance;
 	public int pool = 15;
 	public GameObject barrelPrefab;
-	public List<Barrel> barrels = new List<Barrel>();
+	public List<Barrel> barrels;
 
 	void Awake (){
 		instance = this;
+		barrels = new List<Barrel>(pool);
 	}
 
 	// Use this for initialization
@@ -24,14 +25,14 @@ public class GameManager : MonoBehaviour {
 	}
 
 	private void GeneratePool (){
-		for (int i = 0; i<pool; i++) {
+		for (int i = 0; i < pool; i++) {
 			Barrel barrel = ((GameObject)Instantiate(barrelPrefab, new Vector3(-2.6f,2.22f,0), Quaternion.identity)).GetComponent<Barrel>();
 			barrels.Add (barrel);
 		}
 	}
 
 	public void Roll(){
-		for (int i = 0; i<pool; i++) {
+		for (int i = 0; i < pool; i++) {
 			if (barrels [i].onPool){
 				barrels [i].transform.position = new Vector2 (-1.588f, 2.225f);
 				barrels [i].move = true;
